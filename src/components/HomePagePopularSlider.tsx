d5ab7002533dce5c1b0ec9e5e92448b0f8ca1f5b
apex-devteam <devteam@apexpostech.com>
2023-12-29 11:49:18 -0800

    Added a download button for when an image is popped up.

--- src/components/HomePagePopularSlider.tsx
+++ src/components/HomePagePopularSlider.tsx
@@ -3,6 +3,7 @@ import Image from "./Image";
 import MovieDetailsType from "../models/MovieDetails.types";
 import "../styles/homePagePopularSlider.css"
 import { useNavigate } from "react-router-dom";
+import playIcon from "../images/play-circle.svg"
 
 type HomePagePopularSliderProps = {
     movieData: MovieDetailsType[];
@@ -19,10 +20,10 @@ const HomePagePopularSlider: React.FC<HomePagePopularSliderProps> = ({movieData}
                     <Image imageUrl={movie.backdrop_path}>
                         <div className="movie-info">
                             <div className="movie-title">{movie.title}</div>
-                                <button onClick={() => {
+                                <button className="movie-info-button" onClick={() => {
                                     navigate(`/details/${movie.id}`)
                                 }}>
-                                    More Info
+                                    More Information <img src={playIcon} alt="play-button" className="play-icon"/>
                                 </button>
                         </div>
                     </Image>
--- src/components/ModalImage.tsx
+++ src/components/ModalImage.tsx
@@ -6,6 +6,7 @@ import "../styles/ModalImage.css"
 import { ImageType } from "../models/MovieImages.types";
 import VideoPlayer from "./TrailerVideo";
 import closeButton from "../images/x.svg";
+import downloadIcon from "../images/download.svg"
 
 
 type ModelImage = {
@@ -15,6 +16,48 @@ type ModelImage = {
 }
 
 const ModalImage: React.FC<ModelImage> = ({setOpenModel, openModel, poster}) => {
+
+    // const handleDownloadImage = () => {
+    //     const imageUrl = isImageType(poster)
+    //   ? `https://image.tmdb.org/t/p/original${poster.file_path}`
+    //   : '';
+
+    //   const link = document.createElement('a');
+    //   link.href = imageUrl;
+    //   link.href = imageUrl
+    //   link.download = `download_image`;
+    //   document.body.appendChild(link);
+    //   link.click();
+    //   document.body.removeChild(link);
+    // }   
+    const downloadFile = async () => {
+        const imageUrl = isImageType(poster)
+        ? `https://image.tmdb.org/t/p/original${poster.file_path}`
+        : ""; 
+    
+        if (imageUrl) {
+            try {
+              const response = await fetch(imageUrl);
+              const blob = await response.blob();
+              console.log(blob)
+        
+              // Create a link element
+              const link = document.createElement('a');
+              link.href = window.URL.createObjectURL(blob);
+              link.download = 'downloaded_image';
+        
+              // Simulate a click on the link to trigger the download
+              document.body.appendChild(link);
+              link.click();
+        
+              // Clean up: remove the link from the DOM
+              document.body.removeChild(link);
+            } catch (error) {
+              console.error('Error downloading image:', error);
+            }
+          }
+      }
+
     return (
         <Transition
             show={openModel}
@@ -35,10 +78,12 @@ const ModalImage: React.FC<ModelImage> = ({setOpenModel, openModel, poster}) => 
                             alt="close"
                             onClick={() => setOpenModel(false)}
                         />
-                        <Dialog.Panel className="image-section">
-                            {isImageType (poster) ? <img src={`https://image.tmdb.org/t/p/original${poster.file_path}`} alt="poster"/> : <VideoPlayer video={poster}/>}
-                        </Dialog.Panel>
+                        <div className="image-section">
+                            {isImageType (poster) ? <img className="image-poster" src={`https://image.tmdb.org/t/p/original${poster.file_path}`} alt="poster"/> : <VideoPlayer video={poster}/>}
+                            {isImageType (poster) && <img className="download-button" src={downloadIcon} alt="download" onClick={downloadFile}/>}
+                        </div>
                     </div>
+
             </Dialog>
         </Transition>
     );
